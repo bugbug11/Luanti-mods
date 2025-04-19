6de8e56660c338618e759f44665fc398c60283f2
Oliver Harris <buggyboogles@gmail.com>
2025-04-18 18:36:05 -0600

    Add files via upload

---
+++ hardblocks/init.lua
@@ -0,0 +1,50 @@
+minetest.register_node("hardblocks:1", {
+   description = "Level 1 block",
+   tiles = {"white.png"},
+   groups = {cracky = 1, level = 1},
+})
+minetest.register_node("hardblocks:2", {
+   description = "Level 2 block",
+   tiles = {"white.png"},
+   groups = {cracky = 1, level = 2},
+})
+minetest.register_node("hardblocks:3", {
+   description = "Level 3 block",
+   tiles = {"white.png"},
+   groups = {cracky = 1, level = 3},
+})
+minetest.register_node("hardblocks:4", {
+   description = "Level 4 block",
+   tiles = {"white.png"},
+   groups = {cracky = 1, level = 4},
+})
+minetest.register_node("hardblocks:5", {
+   description = "Level 5 block",
+   tiles = {"white.png"},
+   groups = {cracky = 1, level = 5},
+})
+minetest.register_node("hardblocks:6", {
+   description = "Level 6 block",
+   tiles = {"white.png"},
+   groups = {cracky = 1, level = 6},
+})
+minetest.register_node("hardblocks:7", {
+   description = "Level 7 block",
+   tiles = {"white.png"},
+   groups = {cracky = 1, level = 7},
+})
+minetest.register_node("hardblocks:8", {
+   description = "Level 8 block",
+   tiles = {"white.png"},
+   groups = {cracky = 1, level = 8},
+})
+minetest.register_node("hardblocks:9", {
+   description = "Level 9 block",
+   tiles = {"white.png"},
+   groups = {cracky = 1, level = 9},
+})
+minetest.register_node("hardblocks:10", {
+   description = "Level 10 block",
+   tiles = {"white.png"},
+   groups = {cracky = 1, level = 10},
+})
